4c4fc5a066acf4a0ba27d409446a33475e8b86e9
CNA-YUN <zhangyun20210801@163.com>
2026-04-04 21:33:34 +0800

    根据解题思路自行编写代码

--- Leetcode/problem2087.cpp
+++ Leetcode/problem2087.cpp
@@ -1,23 +1,29 @@
+#include <vector>
+#include <numeric>
+using namespace std;
 class Solution {
 public:
+    int count(vector<int> array, int low, int high) {
+        if (low == high)return 0;
+        int res = 0;
+        while (low < high) {
+            res += array[++low];
+        }
+        while (low > high) {
+            res += array[--low];
+        }
+        return res;
+    }
     int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
         int r1 = startPos[0], c1 = startPos[1];
         int r2 = homePos[0], c2 = homePos[1];
         int res = 0;   // 总代价
         // 移动至家所在行，判断行间移动方向并计算对应代价
-        if (r2 >= r1) {
-            res += accumulate(rowCosts.begin() + r1 + 1, rowCosts.begin() + r2 + 1, 0);
-        }
-        else {
-            res += accumulate(rowCosts.begin() + r2, rowCosts.begin() + r1, 0);
-        }
+        res += count(rowCosts, r1, r2);
+
         // 移动至家所在位置，判断列间移动方向并计算对应代价
-        if (c2 >= c1) {
-            res += accumulate(colCosts.begin() + c1 + 1, colCosts.begin() + c2 + 1, 0);
-        }
-        else {
-            res += accumulate(colCosts.begin() + c2, colCosts.begin() + c1, 0);
-        }
+        res += count(colCosts, c1, c2);
+
         return res;
     }
 };
