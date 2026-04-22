4d52d6a18caf3c989ec198d9e252a68e88cb4e80
CNA-YUN <zhangyun20210801@163.com>
2026-04-22 17:04:35 +0800

    O(26n)

--- Leetcode/problem567.cpp
+++ Leetcode/problem567.cpp
@@ -1,41 +1,50 @@
 /*567. 字符串的排列
-给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的 排列。如果是，返回 true ；否则，返回 false 。
+给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的 排列。如果是，返回
+true ；否则，返回 false 。
 
 排列 是字符串中所有字符的重新排序。
 
 换句话说，s1 的排列之一是 s2 的 子串 。
 */
-#include <iostream>
-#include <vector>
-#include <set>
-#include<unordered_map>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int>hash1;
-        unordered_map<char, int>hash2;
-        for (auto& i : s1) {
-            hash1[i]++;
-        }
-        int k = s1.size();
-        int n = s2.size();
-        for (int j = 0;j < n;j++) {
-            // 
-            hash2[s2[j]]++;
-            int left = j - k + 1;
-            if (left < 0)continue;
-            if (hash1 == hash2)return true;
-            char out = s2[left];
-            if (hash2[out] == 1) {
-                hash2.erase(out);
-            }
-            else {
-                hash2[out]--;
-            }
+        int len1 = s1.size();
+        int len2 = s2.size();
+        if (len1 > len2)
+            return false;
+        bool flag = true;
+        vector<int> map(26, 0);
 
+        for (auto& i : s1) {
+            map[i - 'a']++;
+        }
+
+        for (int j = 0; j < len2; j++) {
+            char in = s2[j];
+            map[in - 'a']--;
+            int left = j - len1 + 1;
+            if (left < 0)
+                continue;
+            flag = true;
+            for (auto& k : map) {
+                if (k != 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+                return true;
+
+            char out = s2[left];
+            map[out - 'a']++;
         }
         return false;
     }
