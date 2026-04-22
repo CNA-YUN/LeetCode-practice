3f0615271a670f335b297f5ac39b1b7e4243163d
CNA-YUN <zhangyun20210801@163.com>
2026-04-22 19:47:48 +0800

    困难；待解决

---
+++ Leetcode/problem30.cpp
@@ -0,0 +1,23 @@
+/*30. 串联所有单词的子串
+给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。
+
+ s 中的 串联子串 是指一个包含  words 中所有字符串以任意顺序排列连接起来的子串。
+ 例如，如果 words = [ab,cd,ef]， 那么 abcdef， abefcd，cdabef， cdefab，efabcd， 和 efcdab 都是串联子串。
+ acdbef 不是串联子串，因为他不是任何 words 排列的连接。
+
+返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
+*/
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <cmath>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+
+    }
+};
